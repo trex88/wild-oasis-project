467540d01eaf1426c56c661cbfd8a18e064be527
trex88 <strawhatluffy199990@gmail.com>
2024-09-12 15:14:11 +1000

    Test reverse

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard() {
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">Dashboard test</Heading>
+        <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
 
