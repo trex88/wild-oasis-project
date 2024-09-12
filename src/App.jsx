3c8ecf5f999a99a085f9bc434318c29e0507641d
trex88 <strawhatluffy199990@gmail.com>
2024-09-12 15:03:31 +1000

    File cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import { DarkModeProvider } from "./context/DarkModeContext";
-import { lazy, Suspense } from "react";
-import Spinner from "./ui/Spinner";
+import { lazy } from "react";
 
 // import Dashboard from "./pages/Dashboard";
 // import Bookings from "./pages/Bookings";
